8558d3adbbd806ccb509bbf4d59a4b7d28c64fce
Viktor Abudikhin <abudihin@gmail.com>
2022-12-10 21:33:34 +0300

    final

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = ""
+}
+
+variable "folder_id" {
+  type    = string
+  default = ""
+}
+
+variable "image_id" {
+  type = string
+  # default = "fd8ibluloco5r4ku4bgg" #alma
+  default = "fd816jiq3n13qtli6fh3" #centos stream
+}
+
+variable "yc_token" {
+  type    = string
+  default = ""
+}
